a83390d0882a6134dfd52c4846aa22e0e69e961f
lemescristina <karen.lemes.cruz@escola.pr.gov.br>
2025-09-04 09:05:36 -0300

    ok

---
+++ script.js
@@ -0,0 +1,16 @@
+const avanca = document.querySelectorA11('.btn-proximo');
+const reiniciarBtn = document.getElementById('.btn-reiniciar');
+
+avanca.array.forEach (button=>{
+button.addEventListener('click',function(){
+   const atual = document.querySelector('.ativo') 
+   const proximoPasso='passo-'+this.getAtteibute('data-proxino');
+
+})
+
+
+}
+
+
+
+)
